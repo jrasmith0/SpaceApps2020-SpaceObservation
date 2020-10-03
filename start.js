5d84ca6eedaa28637978b06720647d9528ebd9a1
jrasmith0 <joshua.smith4@ontariotechu.net>
2020-10-03 11:42:50 -0400

    Changes to location & start.js

--- start.js
+++ start.js
@@ -1,17 +1,17 @@
-var express = require('express');
-var app = express();
-var parser = require('body-parser');
-
+const express = require('express');
+const app = express();
+const parser = require('body-parser');
+const path = require('path');
 
 var urlEncode = parser.urlencoded({extended:false});
 
-//app.use(express.static('resources'));
-//app.set('view engine','pug');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(req,res){
+	res.sendfile(path.join(__dirname, 'public/makeMap.html'));
+});
 
 var server = app.listen(3000,function(){
 	console.log("working");
 });
 
-app.get('/',function(req,res){
-	res.sendfile('makeMap.html');
-});
