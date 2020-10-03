b7b37eed905c7d04f76d224d570a965c56beb77d
jrasmith0 <joshua.smith4@ontariotechu.net>
2020-10-03 17:15:49 -0400

    changing process env port

--- start.js
+++ start.js
@@ -13,7 +13,7 @@ app.get('/',function(req,res){
 	res.render(path.join(__dirname, 'public/makeMap'));
 });
 
-var server = app.listen(3000,function(){
+var server = app.listen(process.env.PORT || 3000,function(){
 	console.log("working");
 });
 
