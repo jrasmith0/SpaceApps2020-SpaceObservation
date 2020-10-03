992b08324ed9b0e3640cf845910eb6bbffc39ad4
Shayne Taylor <shayne@Shaynes-MacBook-Pro.local>
2020-10-03 14:42:40 -0400

    changed HTML to PUG templating

--- start.js
+++ start.js
@@ -5,10 +5,12 @@ const path = require('path');
 
 var urlEncode = parser.urlencoded({extended:false});
 
+app.set('view engine','pug');
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/',function(req,res){
-	res.sendfile(path.join(__dirname, 'public/makeMap.html'));
+	res.render(path.join(__dirname, 'public/makeMap'));
 });
 
 var server = app.listen(3000,function(){
