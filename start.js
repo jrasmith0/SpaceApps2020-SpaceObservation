1aec2d71814b3578e3bf8b09ff469f8c35d02b89
Shayne Taylor <shayne@Shaynes-MacBook-Pro.local>
2020-10-03 18:50:32 -0400

    added functionality to parse NEOSSAT data to find location and title of objects

--- start.js
+++ start.js
@@ -2,16 +2,21 @@ const express = require('express');
 const app = express();
 const parser = require('body-parser');
 const path = require('path');
+const csv = require('csvtojson'); 
 
 var urlEncode = parser.urlencoded({extended:false});
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine','pug');
-
+json = "";
 
 
 app.get('/',function(req,res){
-	res.render('makeMap');
+	csv().fromFile('public/data.csv').then((jsonObj)=>{
+		json = jsonObj;
+		console.log(jsonObj);
+	})
+	res.render('makeMap', {jsonList: json});
 });
 
 var server = app.listen(process.env.PORT || 3000,function(){
